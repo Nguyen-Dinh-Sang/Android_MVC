4c274a92c7356ab26422aee9e089097da864bb3e
DinhSang <dinhsangdroid@gmail.com>
2021-03-12 16:32:54 +0700

    MVC-DinhSang: login ui

--- app/src/main/java/com/example/android_mvc/login/LoginActivity.java
+++ app/src/main/java/com/example/android_mvc/login/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.android_mvc;
+package com.example.android_mvc.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android_mvc.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
